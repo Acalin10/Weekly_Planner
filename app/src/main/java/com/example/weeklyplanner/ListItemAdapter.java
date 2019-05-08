b5c7624aa08f20c149df060b217b398e02d52bbb
David <dmocko18@student.aau.dk>
2019-05-08 10:15:36 +0200

    ShoppingList.java lines 102-118 res>layout>menu>list_top_navigation.xml

--- app/src/main/java/com/example/weeklyplanner/ListItemAdapter.java
+++ app/src/main/java/com/example/weeklyplanner/ListItemAdapter.java
@@ -106,8 +106,6 @@ public class ListItemAdapter extends BaseAdapter {
                 ShoppingList.saveArray(getList_items_days(),getSavedays());
             }
         });
-
-
         String name = getList_items().get(position);
         String day = getList_items_days().get(position);
         nameTextView.setText(name);
